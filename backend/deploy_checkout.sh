f357a79a43ad28285bc9a43a8c2d165f51769775
anderson xavier <andersonxavier83@gmail.com>
2023-05-21 03:42:40 -0300

    adding github workflow

--- backend/deploy_checkout.sh
+++ backend/deploy_checkout.sh
@@ -5,7 +5,9 @@ set -e
 # Deixar o mesmo nome q esta em build_resources.sh pq ele puxa pelo nome
 APPNAME="ecommerceappcursoax"
 
-cd ../   #Volta um diretorio para compilar e subir.
+#cd ../   #Volta um diretorio para compilar e subir.
+cd ../src  # Mudei os arquivos do projeto template para o src!
+
 # Aqui usa o Azure core function que instalamos
 # Publica um codigo para functional APP
 func azure functionapp publish $APPNAME
